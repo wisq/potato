89bbf19cd73660b132c2de78d0a9dcd3cfea0c38
Adrian Irving-Beer <adrian@shopify.com>
2011-05-16 23:13:10 -0400

    Use SIGHUP to restart pinger.

--- bin/pinger.rb
+++ bin/pinger.rb
@@ -21,43 +21,66 @@ class Pinger
     puts "Pinger starting."
     pinger = Pinger.new(nic)
     puts "Pinger ready."
-    pinger.ping
+
+    begin
+      pinger.ping
+    rescue SignalException => e
+      raise e unless e.signm == 'SIGHUP'
+
+      puts "SIGHUP received, restarting ping."
+      retry
+    end
   end
 
   def initialize(nic)
     @nic = nic
 
+    @ppp = Potato::PPP.new
+
     @redis = Redis.new
     @redis_key = "ping-#{nic}"
 
-    @last_seq = 0
     @last_status = Time.now.min
-  end
-
-  def ping
-    ppp = Potato::PPP.new
-    until iface = ppp.find_by_nic(@nic)
-      puts "Interface #{@nic} not attached yet."
-      sleep(30)
-    end
 
     if old_lost = @redis.lindex(@redis_key, 0)
       puts "Resuming old ping with #{old_lost} lost packets."
     end
     @lost = old_lost.to_i
+  end
+
+  def ping
+    reap_old_pings
+
+    until iface = @ppp.find_by_nic(@nic)
+      puts "Interface #{@nic} not attached yet."
+      sleep(30)
+    end
 
     target = iface.ip_remote
     device = iface.device
 
+    @last_seq = 0
     launch_ping(target, device)
     launch_timeout
 
     receive_pings
   ensure
+    @timeout.kill if @timeout
     Process.kill('TERM', @ping_pid) if @ping_pid
+
+    @timeout  = nil
+    @ping_pid = nil
   end
 
   private
+
+  # Reap any old zombie ping processes.
+  # If we do this during the ensure, a double-signal
+  # can leave zombies lying around.
+  def reap_old_pings
+    loop { Process.waitpid(-1, Process::WNOHANG) }
+  rescue Errno::ECHILD
+  end
 
   def launch_ping(target, device)
     puts "Pinging #{target} on #{device}."
@@ -83,7 +106,7 @@ class Pinger
     @expected = {1 => Time.now + INTERVAL + TIMEOUT}
 
     Thread.abort_on_exception = true
-    Thread.new do
+    @timeout = Thread.new do
       loop do
         now = Time.now
         @expected.dup.each do |seq, time|
